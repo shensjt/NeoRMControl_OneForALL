64f703f3509f6fe1aec0a97dda267d5551347e58
tracer-222023321262021-沈俊陶 <tracer@gkd>
2024-05-21 20:00:55 +0800

    > compile wormup!! 222023321262021 沈俊陶 Linux shenchen 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:00:55 up 41 min,  1 user,  load average: 0.10, 0.08, 0.04

--- 3rdparty/newbee/stage2-clowncards/src/main.cc
+++ 3rdparty/newbee/stage2-clowncards/src/main.cc
@@ -1,21 +1,13 @@
 #include <iostream>
-#include <time.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-int main(){
-  for(int i = 0;i< 10 ;i++){
+int main(void){
+  for(int i = 0; i< 10; i++){
 
-    printf("\033[2J");
-
-    for(int j = 0;j <10;j++ ){
-      std::cout << " ";
-    }
-    std::cout << "A" << std::endl;
-
-    sleep(1);
-
+    system("clear");
+    std::cout << "test :  " << i << std::endl;
   }
+
+
 
   return 0;
 }
