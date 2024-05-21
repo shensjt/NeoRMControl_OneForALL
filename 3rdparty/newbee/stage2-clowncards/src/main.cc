c9d00a49108bde9666ae6adef706e7936af734f8
tracer-222023321262021-沈俊陶 <tracer@gkd>
2024-05-21 19:53:44 +0800

    > compile wormup!! 222023321262021 沈俊陶 Linux shenchen 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:53:44 up 34 min,  1 user,  load average: 0.12, 0.08, 0.04

--- 3rdparty/newbee/stage2-clowncards/src/main.cc
+++ 3rdparty/newbee/stage2-clowncards/src/main.cc
@@ -1,61 +1,21 @@
 #include <iostream>
-#include <stack>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#include <vector>
-using namespace std;
-const int N = 1000;
+int main(){
+  for(int i = 0;i< 10 ;i++){
 
-vector<int> g1[N], g2[N];
-bool vis1[N], vis2[N];
-stack<int> st;
+    printf("\033[2J");
 
-void addEdge(int x, int y) {
-  g1[x].push_back(y);
-  g2[y].push_back(x);
+    for(int j = 0;j <10;j++ ){
+      std::cout << " ";
+    }
+    std::cout << "A" << std::endl;
+
+    sleep(1);
+
+  }
+
+  return 0;
 }
-
-void dfs1(int x) {
-  if (vis1[x]) {
-    return;
-  }
-  vis1[x] = 1;
-  for (auto y : g1[x]) {
-    dfs1(y);
-  }
-  st.push(x);
-}
-
-void dfs2(int x) {
-  if (vis2[x]) {
-    return;
-  }
-  cout << x << ' ';
-  vis2[x] = 1;
-  for (auto y : g2[x]) {
-    dfs2(y);
-  }
-}
-
-int main() {
-  int n = 7;
-  addEdge(1, 0);
-  addEdge(2, 1);
-  addEdge(2, 5);
-  addEdge(3, 2);
-  addEdge(0, 3);
-  addEdge(0, 4);
-  addEdge(4, 3);
-  addEdge(5, 7);
-  addEdge(7, 6);
-  addEdge(6, 5);
-  for (int i = 0; i < n; i++) {
-    dfs1(i);
-  }
-  while (!st.empty()) {
-    dfs2(st.top());
-    st.pop();
-    std::cout << '\n';
-  }
-  std::cout << "hi" << std::endl;
-}
-
