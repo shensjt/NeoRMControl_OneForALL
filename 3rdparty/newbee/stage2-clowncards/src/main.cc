b28223a356bcbe5f998cf9f9f0150dda33208b73
tracer-222023321262021-沈俊陶 <tracer@gkd>
2024-05-21 21:18:51 +0800

    > compile wormup!! 222023321262021 沈俊陶 Linux shenchen 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:18:51 up  1:59,  1 user,  load average: 0.07, 0.09, 0.09

--- 3rdparty/newbee/stage2-clowncards/src/main.cc
+++ 3rdparty/newbee/stage2-clowncards/src/main.cc
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <string>
 
+int nums[9] = {0};
+
+void set_card(){
+
+
+}
+
+
 int main(){
 
   std::string a[4] = {"♠", "♥", "♣", "♦"};
 
+
+
+
   for(int i = 0; i < 4;i++){
 
-    std::cout << a[i] << std::endl;
-    
+    std::cout << a[i] << ":" << nums[i] << std::endl;
+
   }
 
 
