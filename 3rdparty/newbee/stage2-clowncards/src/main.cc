5bf2579cda936f68311db37cba4371585fa3ac16
tracer-222023321262021-沈俊陶 <tracer@gkd>
2024-05-21 21:30:17 +0800

    > compile wormup!! 222023321262021 沈俊陶 Linux shenchen 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:30:17 up  2:11,  1 user,  load average: 0.00, 0.03, 0.06

--- 3rdparty/newbee/stage2-clowncards/src/main.cc
+++ 3rdparty/newbee/stage2-clowncards/src/main.cc
@@ -1,24 +1,42 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <cstdlib>
+
 
 int nums[9] = {0};
+std::string suits[9];
 
-void set_card(){
-
-
-}
+void set_card(){}
 
 
 int main(){
+  srand((unsigned int)time(nullptr));
 
   std::string a[4] = {"♠", "♥", "♣", "♦"};
 
+  for(int i = 0 ;i<9;i++){
+    if(nums[i] == 0){
+      int temp;
+      nums[i] = 1 + rand()%13;
+      temp = rand()%4;
+      suits[i] == a[temp];
+    }
+
+    for(int x = 0;x<9;x++){
+      if(x = i){
+        continue;
+      }
+      if(nums[i] == nums[x] && suits[i] == suits[x]){
+        i--;
+      }
+    }
+  }
 
 
+  for(int i = 0; i < 9;i++){
 
-  for(int i = 0; i < 4;i++){
-
-    std::cout << a[i] << ":" << nums[i] <<" ";
+    std::cout << suits[i] << ":" << nums[i] << std::endl;;
 
   }
 
