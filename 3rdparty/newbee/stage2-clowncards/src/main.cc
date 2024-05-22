1207948b76dea61b1b88ca674faa8e123f07794c
tracer-222023321262021-沈俊陶 <tracer@gkd>
2024-05-22 15:07:44 +0800

    > compile NeoRMControl!! 222023321262021 沈俊陶 Linux shenchen 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:07:44 up  1:07,  1 user,  load average: 0.23, 0.19, 0.18

--- 3rdparty/newbee/stage2-clowncards/src/main.cc
+++ 3rdparty/newbee/stage2-clowncards/src/main.cc
@@ -1,10 +1,46 @@
 #include "header.h"
 
+std::string SUIT[4] = {"♠", "♥", "♣", "♦"};
+int num[9] = {0};
+std::string suit[9] = {" "};
+void INI_CARD(){
+  for(int i = 0; i<9;i++){
+    if(num[i == 0]){
+      num[i] = 1 + rand()%13;
+      int temp = rand()%4;
+      suit[i] = SUIT[temp];
+      for(int x = 0;x<9;x++){
+        if(x == i){
+            continue;
+        }
+        if(num[i] == num[x] && suit[i] == suit[x]){
+          num[i] = 0;
+          i = i-1;
+        }
+      }
+    }
+  }
+}
+
+
 int main(){
 
   std::srand((unsigned int)time(nullptr));
 
-  test_show(card);
+  INI_CARD();
+
+  for(int i ;i<9;i++){
+    std::cout << suit[i] << ":" << num[i] << std::endl;
+  }
+
+  num[2] = 0;
+  num[3] = 0;
+
+  INI_CARD();
+
+   for(int i ;i<9;i++){
+    std::cout << suit[i] << ":" << num[i] << std::endl;
+  }
 
   return 0;
 }
