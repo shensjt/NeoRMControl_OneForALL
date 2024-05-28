76753679686264c5badc5c19ab8d701ca4d6b6a8
shensjt <2052780302@qq.com>
2024-05-28 15:06:26 +0800

    modified:   3rdparty/newbee/stage2-clowncards/include/header.h 	modified:   3rdparty/newbee/stage2-clowncards/src/main.cc

--- 3rdparty/newbee/stage2-clowncards/src/main.cc
+++ 3rdparty/newbee/stage2-clowncards/src/main.cc
@@ -1,61 +1,55 @@
-#include <iostream>
-#include <stack>
+#include "header.h"
 
-#include <vector>
-using namespace std;
-const int N = 1000;
 
-vector<int> g1[N], g2[N];
-bool vis1[N], vis2[N];
-stack<int> st;
 
-void addEdge(int x, int y) {
-  g1[x].push_back(y);
-  g2[y].push_back(x);
+
+void test_show(CARD card) {
+
+    for (int i = 0; i < 8; i++) {
+
+        std::cout << card.get_suit(i) << ":" << card.get_num(i) << "  ";
+
+    }
+    printf("\n");
 }
 
-void dfs1(int x) {
-  if (vis1[x]) {
-    return;
-  }
-  vis1[x] = 1;
-  for (auto y : g1[x]) {
-    dfs1(y);
-  }
-  st.push(x);
-}
 
-void dfs2(int x) {
-  if (vis2[x]) {
-    return;
-  }
-  cout << x << ' ';
-  vis2[x] = 1;
-  for (auto y : g2[x]) {
-    dfs2(y);
-  }
-}
 
-int main() {
-  int n = 7;
-  addEdge(1, 0);
-  addEdge(2, 1);
-  addEdge(2, 5);
-  addEdge(3, 2);
-  addEdge(0, 3);
-  addEdge(0, 4);
-  addEdge(4, 3);
-  addEdge(5, 7);
-  addEdge(7, 6);
-  addEdge(6, 5);
-  for (int i = 0; i < n; i++) {
-    dfs1(i);
-  }
-  while (!st.empty()) {
-    dfs2(st.top());
-    st.pop();
-    std::cout << '\n';
-  }
-  std::cout << "hi" << std::endl;
-}
 
+
+
+
+int main(void) {
+
+    std::srand((unsigned int)time(nullptr));
+    std::vector<int> input_int; // 输入值处理后得到整数
+    std::string input; // 输入的存放
+    do{
+        card.reset_discard();
+        card.reset_score();
+    do{
+        // 刷新屏幕
+        clearScreen();
+
+        // 初始化与更新
+        card.INI_CARD();
+
+        // 界面展示
+        window();
+        for(int number : input_int){
+            std::cout << number << " " << std::endl;
+        }
+
+        // 输入
+
+        input_get(input_int, input);
+
+
+
+    } while(input != "q");
+
+
+    }while(input != "q" ); // 重复游戏选项（未完成）
+
+    return 0;
+}
