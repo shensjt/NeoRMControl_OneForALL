4c71cedea90bdf81aa645ec79f4063dd34b2d916
shensjt <2052780302@qq.com>
2024-07-13 18:18:09 +0800

    modified:   3rdparty/newbee/stage2-clowncards/include/header.h 	modified:   3rdparty/newbee/stage2-clowncards/src/main.cc

--- 3rdparty/newbee/stage2-clowncards/src/main.cc
+++ 3rdparty/newbee/stage2-clowncards/src/main.cc
@@ -1,54 +1,14 @@
 #include "header.h"
 
-void test_show(CARD card) {
-    for (int i = 0; i < 8; i++) {
-        std::cout << card.get_suit(i) << ":" << card.get_num(i) << "  ";
-    }
-    printf("\n");
-}
-
-int main(void) {
+int main() {
+    CARD card;
     std::srand((unsigned int)time(nullptr));
-    std::vector<int> input_int;  // 输入值处理后得到整数
-    std::string input;           // 输入的存放
 
-    do {
-        card.new_round();
-        do {
-            // 刷新屏幕
-            clearScreen();
-            
-            // 初始化与更新
-            card.INI_CARD();
-            
-            // 界面展示
-            card.window();
-            for (int number : input_int) {
-                std::cout << number << " " << std::endl;
-            }
+    bool game_check = true;
 
-            // 判断游戏进程
-            if (card.get_is_over() == true) {
-                char check;
-                std::cin >> check;
-                if (check == 'y') {
-                    card.new_game();
-                    break;
-                } else {
-                    input = "q";
-                    break;
-                }
-            }
-            // 输入
-            card.new_usecard();
-            card.input_get(input_int, input);
-            if(card.score_update()){
-                break;
-            }
-
-        } while (input != "q");  // 更换小回合判断条件
-
-    } while (input != "q");  // 重复游戏选项（未完成）
-
+   
+    while (game_check) { 
+        game_check = card.game_loop();
+    }
     return 0;
 }
